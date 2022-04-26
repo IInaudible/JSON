77d71a077bbe5b80c4df08edf500e37bca5b3406
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-04-26 13:12:48 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+//convert the following objects to JSON
+
+let power={voltage: 250,current: 12}
+
+let car={make: "Honda", model: "Accord", year: 2017}
+
+let person={name: "Bob", age:45, email:"jim@email.com", occupation: "teacher"}
+
+// To illustrate how to use the JSON editor Tool we are going to create a JSON string with an array of 3 objects.The objects are:
+
+// Object 1 contains the following key/value pairs:
+
+// name=John,DOB=1/1/2000,Age=20
+
+// Object 2 contains the following key/value pairs:
+
+// name=Jim,DOB=21/2/1990,Age=30
+
+// Object 3 contains the following key/value pairs: 
+
+//name=Jane,DOB=6/11/1958,Age=61
+
+//console.log the name in each object using JSON.parse
